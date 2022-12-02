a2f90be5dbfe309291e2828be54c7639d02eb535
Birthright00 <117997217+Birthright00@users.noreply.github.com>
2022-12-02 10:16:36 +0800

    Add files via upload

--- Testing with Image using def().py
+++ Testing with Image using def().py
@@ -1,6 +1,11 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import os
+import random
+
+selfie_image = r"C:\\Users\\Edward\\Downloads\\ame sprites\\we will use\\Selfies"
+disappointed_image = r"C:\\Users\\Edward\\Downloads\\ame sprites\\we will use\\Disappointed"
+asking_image = r"C:\\Users\\Edward\\Downloads\\ame sprites\\we will use\\Asking"
 
 win = Tk()
 win.title("Finish?")
@@ -8,33 +13,47 @@ win.geometry("500x500")
 frame = Frame(win, width=600, height=400)
 frame.pack()
 frame.place(anchor='center', relx=0.5, rely=0.5)
-img = ImageTk.PhotoImage(Image.open("C:\\Users\\Edward\\Downloads\\ame sprites\\stream_ame_talk_003_0.png"))
+r = random.choice(os.listdir(asking_image))
+img=ImageTk.PhotoImage(Image.open(os.path.join(asking_image, r)))
 label = Label(frame, image = img)
-image = img
 label.pack()
 
 def beginning():
     button = Button(win, text='Did you finish?', command=open_img).pack()
-
+ 
 def open_img():
     clear_frame()
-    btn1=Button(win, text="Yes!", fg='blue', command =lambda: btn1_clicked)
-    btn1.place(x=80, y=100)
-    btn2=Button(win, text="No!", fg='red', command =lambda: btn2_clicked)
-    btn2.place(x=200, y=100)
+    btn1=Button(win, text="Yes!", fg='blue', command =btn1_clicked)
+    btn1.place(x=80, y=50)
+    btn2=Button(win, text="No!", fg='red', command =btn2_clicked)
+    btn2.place(x=360, y=50)
 
 
 def btn1_clicked():
+    clear_frame()
     win.title("Good boy! ^w^")
-    img1=ImageTk.PhotoImage(Image.open("C:\\Users\\Edward\\Downloads\\twitter selfies\\twitter selfies\\tweet_selfie_ame_happy_001.png"))
-    label.configure(img=img1)
-    image = img1
+    selfie()
 
 
 
-def btn2_clicked(img):
-    win.title("Bad boy...")
-    label['img'] = ImageTk.PhotoImage("C:\\Users\\Edward\\Downloads\\ame sprites\\stream_ame_tv_004.png")
+def btn2_clicked():
+    clear_frame()
+    win.title("How dare you...")
+    disappointed()
+
+def selfie():
+    global img
+    r = random.choice(os.listdir(selfie_image))
+    img=ImageTk.PhotoImage(Image.open(os.path.join(selfie_image, r)))
+    label = Label(frame, image = img)
+    label.pack()
+
+def disappointed():
+    global img
+    r = random.choice(os.listdir(disappointed_image))
+    img=ImageTk.PhotoImage(Image.open(os.path.join(disappointed_image, r)))
+    label = Label(frame, image = img)
+    label.pack()
 
 def clear_frame():
    for widgets in frame.winfo_children():
