40b7796f5399472f8e7777af151566bf151fac68
Haowen <piersonhaowenlb@gmail.com>
2022-12-07 05:23:29 +0800

    new files

--- test.py
+++ test.py
@@ -39,7 +39,6 @@ with open('./dialogues.json') as json_file:
     data = json.load(json_file)
     diag_small_talk = data["small_talk"]
     diag_asking = data["asking"]
-    diag_completed = data["completed"]
     diag_failed = data["failed"]
     diag_questions = data["questions"]
 
@@ -151,7 +150,7 @@ def main():
     init_ame()
     start_random_event(600)
     tk_root.mainloop()
-    #dev_console()
+    dev_console()
     
 
 
@@ -176,8 +175,7 @@ def init_ame():
             affection = data["character"]["affection"]
             if data["tasks"]:
                 tasks = data["tasks"]
-                for task in tasks:
-                    start_task_timer(task["name"], task["time"])
+                #add tasks to gui
             ame = Ame(name, happiness, affection)
             tk_happiness_stat.set(ame.happiness)
             tk_affection_stat.set(ame.affection)
@@ -197,7 +195,6 @@ def write_save():
     with open('./save.json', 'w') as outfile:
         json.dump(data, outfile)
 
-#fully functional terminal console, replaced by GUI dev console
 def dev_console():
     while True:
         i = int(input("\nDev console\n\n1: Show stats\n2: Modify stats\n3: List tasks\n4: Create task\n5: Delete task\n6: Random event interval\n7: Start random events\n8: Stop random events\n99: Exit\nEnter an option: "))
@@ -244,29 +241,27 @@ def update_char_name(name):
 def create_task(name, time):
     for task in tasks:
         if name in task["name"]:
-            return("Enter another name")
+            return("\n###Enter another name###\n")
     try:
-        time = int(time)
+        time = float(time)
     except ValueError:
-        return("Enter a number")
+        return("###Enter a number###")
     tasks.append({"name": name, "time": time})
-    start_task_timer(name, time)
-    init_task_elements()
     write_save()
 
-def delete_task(task_name):
-    del_task = (next((index for (index, d) in enumerate(tasks) if d["name"] == task_name), None))
-    if isinstance(del_task, int):
+def delete_task(name):
+    del_task = (next((index for (index, d) in enumerate(tasks) if d["name"] == name), None))
+    if del_task:
         tasks.pop(del_task)
     else:
-        print("Task not found")
+        return "\n###Task not found###\n"
     write_save()
 
 def get_stat_update(stat_type, old, new):
     stages = ["0", "1-25", "25-50", "50-75", "75-99", "100"]
     old_stage = bisect.bisect([1, 25, 50, 75, 100], old)
     new_stage = bisect.bisect([1, 25, 50, 75, 100], new)
-    if stat_type == "happiness":
+    if old_stage != new_stage:
         update_main(new_stage)
     return [stat_type, old_stage, new_stage]
 
@@ -309,26 +304,23 @@ main_dialogue_text = ""
 
 def init():
     global main_sprite
-    notice_box.grid(row=99, column=0)
-    init_console()
     if new_save:
-        main_frame.grid(row=1, column=0)
+        main_frame.grid(row=2, column=0)
         main_dialogue_text = "What do you want to call me uWu?~~"
         main_dialogue_label.config(text = main_dialogue_text)
         main_sprite_display = tkinter.Label(main_frame, image = main_sprite)
         main_sprite_display.grid(row=0, column=0)
         main_dialogue_label.grid(row=1, column=0)
-        name_entry.grid(row=2, column=0)
-        name_button.grid(row=3, column=0)
+        main_entry1.grid(row=2, column=0)
+        main_button1.grid(row=3, column=0)
     else:
         init_main()
 
 def submit_name():
-    if name_entry.get():
-        update_char_name(name_entry.get())
-        name_button.grid_forget()
-        name_entry.grid_forget()
+    if main_entry1.get():
+        update_char_name(main_entry1.get())
         init_main()
+        update_main(3)
     
 def init_main():
     stat_bar_frame.grid(row=0, column=0)
@@ -338,8 +330,8 @@ def init_main():
     affection_bar.grid(row=1, column=1, padx = 10, pady = 10)
     affection_sprite_display = tkinter.Label(stat_bar_frame, image = affection_sprite)
     affection_sprite_display.grid(row=1, column=0, padx = 10, pady = 10)
-    ame.happiness += 0 #refresh stat to update
-    init_task_elements()
+    stage = bisect.bisect([1, 25, 50, 75, 100], ame.happiness)
+    update_main(stage)
 
 def update_gui_stats():
     happiness_sprite_display = tkinter.Label(stat_bar_frame, image = happy_sprite)
@@ -353,7 +345,6 @@ def update_gui_stats():
 
 def update_main(stage):
     global main_sprite
-    notice_box.config(text="")
     dir = r"./resources/stage_" + str(stage)
     r = random.choice(os.listdir(dir))
     main_sprite = ImageTk.PhotoImage(Image.open(os.path.join(dir, r)).resize((522, 340)))
@@ -362,145 +353,8 @@ def update_main(stage):
     main_dialogue_text = random.choice(diag_small_talk)
     main_dialogue_label.config(text = main_dialogue_text)
     main_dialogue_label.grid(row=1, column=0)
-
-def init_task_elements():
-    task_frame.grid_forget()
-    task_name_listbox.delete(0, END)
-    task_time_listbox.delete(0, END)
-    task_frame.grid(row=2, column=0)
-    task_entry_frame.grid(row=0, column=0, padx=10, pady=10)
-    task_name_entry.grid(row=0, column=0)
-    task_time_entry.grid(row=0, column=1)
-    task_create_button.grid(row=1, column=0)
-
-    task_list_frame.grid(row=2, column=0, padx=10, pady=10)
-
-    if tasks:
-        for task in tasks:
-            task_name_listbox.insert(tkinter.END, task["name"])
-            task_time_listbox.insert(tkinter.END, task["time"])
-
-    task_title_name.grid(row=0, column=0)
-    task_time_name.grid(row=0, column=1)
-    task_name_listbox.grid(row=1, column=0)
-    task_time_listbox.grid(row=1, column=1)
-
-    task_list_btn_frame.grid(row=3, column=0)
-    task_complete_button.grid(row=0, column=0)
-    task_fail_button.grid(row=0, column=1)
-
-
-def submit_task():
-    if task_name_entry.get() and task_time_entry.get():
-        message = create_task(task_name_entry.get(), task_time_entry.get())
-        if message:
-            notice_box.config(text=message)
-        else:
-            notice_box.config(text="")
-
-def complete_task():
-    global main_sprite
-    if task_name_listbox.curselection():
-        task_frame.grid_forget()
-        task_entry_frame.grid_forget()
-        task_list_frame.grid_forget()
-        task_list_btn_frame.grid_forget()
-
-        if ame.happiness >= 50:
-            r = random.choice(os.listdir(complete_kangel))
-            main_sprite = ImageTk.PhotoImage(Image.open(os.path.join(complete_kangel, r)).resize((522, 340)))
-        elif ame.happiness < 50:
-            r = random.choice(os.listdir(complete_ame))
-            main_sprite = ImageTk.PhotoImage(Image.open(os.path.join(complete_ame, r)).resize((522, 340)))
-        main_dialogue_text = random.choice(diag_completed)
-        main_dialogue_label.config(text = main_dialogue_text)
-        main_sprite_display = tkinter.Label(main_frame, image = main_sprite)
-        main_sprite_display.grid(row=0, column=0)
-
-        event_frame.grid(row=3, column=0)
-        event_button.config(text="yay~~~", command= lambda: complete_event("complete"))
-        event_button.grid(row=0, column=0)
-        for i in task_name_listbox.curselection():
-            sel = task_name_listbox.get(i)
-            print("here")
-            delete_task(sel)
-
-def fail_task():
-    global main_sprite
-    if task_name_listbox.curselection():
-        task_frame.grid_forget()
-        task_entry_frame.grid_forget()
-        task_list_frame.grid_forget()
-        task_list_btn_frame.grid_forget()
-
-        if ame.happiness >= 50:
-            r = random.choice(os.listdir(fail_kangel))
-            main_sprite = ImageTk.PhotoImage(Image.open(os.path.join(fail_kangel, r)).resize((522, 340)))
-        elif ame.happiness < 50:
-            r = random.choice(os.listdir(fail_ame))
-            main_sprite = ImageTk.PhotoImage(Image.open(os.path.join(fail_ame, r)).resize((522, 340)))
-
-        main_dialogue_text = random.choice(diag_failed)
-        main_dialogue_label.config(text = main_dialogue_text)
-        main_sprite_display = tkinter.Label(main_frame, image = main_sprite)
-        main_sprite_display.grid(row=0, column=0)
-
-        event_frame.grid(row=3, column=0)
-        event_button.config(text="I'm sorry...", command= lambda: complete_event("fail"))
-        event_button.grid(row=0, column=0)
-        for i in task_name_listbox.curselection():
-            sel = task_name_listbox.get(i)
-            delete_task(sel)
-
-def complete_event(status):
-    event_frame.grid_forget()
-    if status == "complete":
-        ame.happiness += 3
-    elif status == "fail":
-        ame.happiness -= 3
-    elif status == "yes":
-        ame.affection += 5
-    elif status == "no":
-        ame.affection -= 5
-    init_task_elements()
-
     
-def init_console():
-    console_frame.grid(row=1, column=1)
-    console_title.grid(row=1, column=0)
-    console_listbox.grid(row=2, column=0,  padx = 10, pady = 10)
-    console_status.grid(row=3, column=0)
-    console_entry.grid(row=4, column=0)
-    console_button.grid(row=5, column=0)
-
-def submit_command():
-    for i in console_listbox.curselection():
-        sel = console_listbox.get(i)
-        if sel == "Show stats":
-            console_status.config(text=f"Name is {ame.name}, happiness is {ame.happiness}, affection is {ame.affection}")
-        if sel == "Modify happiness":
-            if console_entry.get() and console_entry.get().isnumeric():
-                ame.happiness = int(console_entry.get())
-        if sel == "Modify affection":
-            if console_entry.get() and console_entry.get().isnumeric():
-                ame.affection = int(console_entry.get())
-        if sel == "Delete task":
-            if console_entry.get():
-                message = delete_task(console_entry.get())
-                if message:
-                    console_status.config(text= message)
-        if sel == "Set random event interval":
-            if console_entry.get() and console_entry.get().isnumeric():
-                random_event_timer.cancel()
-                start_random_event(float(console_entry.get()))
-        if sel == "Start random events":
-            if random_event_timer:
-                console_status.config(text="Random event running")
-            else:
-                start_random_event(600)
-        if sel == "Stop random events":
-            random_event_timer.cancel()
-
+    
 
 #status bar
 stat_bar_frame = tkinter.Frame(tk_root)
@@ -514,56 +368,17 @@ affection_sprite = ImageTk.PhotoImage(Image.open(affection_icon).resize((25, 25)
 main_frame = tkinter.Frame(tk_root)
 main_sprite = ImageTk.PhotoImage((Image.open(init_sprite).resize((522, 340))))
 main_dialogue_label = Label(main_frame, text = main_dialogue_text, padx = 10, pady = 10)
-name_entry = Entry(main_frame)
-name_button = Button(main_frame, text= "Submit", padx = 20, command=submit_name)
+main_entry1 = Entry(main_frame)
+main_button1 = Button(main_frame, text= "Submit", padx = 20, command=submit_name)
+tk_root.after_idle(init)
 
 #task elements
 task_frame = tkinter.Frame(tk_root)
-task_entry_frame = tkinter.Frame(task_frame)
-task_name_entry = Entry(task_entry_frame)
-task_name_entry.insert(0, "Enter task name")
-task_time_entry = Entry(task_entry_frame)
-task_time_entry.insert(0, "Enter task time")
-task_create_button = Button(task_frame, text= "Submit", padx = 20, command=submit_task)
-
-task_list_frame = tkinter.Frame(task_frame)
-task_title_name = Label(task_list_frame, text ="Name", padx = 10, pady = 10)
-task_time_name = Label(task_list_frame, text ="Time", padx = 10, pady = 10)
-task_name_listbox = Listbox(task_list_frame, width=30, height=10, selectmode=SINGLE)
-task_time_listbox = Listbox(task_list_frame, width=10, height=10, selectmode=DISABLED)
-
-task_list_btn_frame = tkinter.Frame(task_frame)
-task_complete_button = Button(task_list_btn_frame, text= "Complete", padx = 20, command=complete_task)
-task_fail_button = Button(task_list_btn_frame, text= "Fail", padx = 20, command=fail_task)
-
-#task event elements
-event_frame = tkinter.Frame(tk_root)
-event_button = Button(event_frame, text= "Submit", padx = 20, command= lambda: complete_event(""))
-event_button_yes = Button(event_frame, text= "Yes", padx = 20, command= lambda: complete_event("yes"))
-event_button_no = Button(event_frame, text= "Yes", padx = 20, command= lambda: complete_event("no"))
 
 #dev console elements
 console_frame = tkinter.Frame(tk_root)
-console_title = Label(console_frame, text ="Dev console", padx = 10, pady = 10)
+console_label_text = "\nDev console\n\n1: Show stats\n2: Modify stats\n3: List tasks\n4: Create task\n5: Delete task\n6: Random event interval\n7: Start random events\n8: Stop random events\nEnter an option:"
 
-console_listbox = Listbox(console_frame, width=30, height=10, selectmode=SINGLE)
-console_listbox.insert(1, "Show stats")
-console_listbox.insert(2, "Modify happiness")
-console_listbox.insert(3, "Modify affection")
-console_listbox.insert(4, "Delete task")
-console_listbox.insert(5, "Set random event interval")
-console_listbox.insert(6, "Start random events")
-console_listbox.insert(7, "Stop random events")
-
-
-console_status = Label(console_frame, text = "", padx = 10, pady = 10)
-console_entry = Entry(console_frame)
-console_button = Button(console_frame, text= "Submit", padx = 20, command=submit_command)
-
-#notice box to handle errors
-notice_box = Label(tk_root, text = "", padx = 10, pady = 10)
-
-tk_root.after_idle(init)
 
 #init main(), put this last
 if __name__ == "__main__":
